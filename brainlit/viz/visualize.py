7555b3615032481efd2ebc49e73f9b6d31fbc124
Thomas Athey <tom.l.athey@gmail.com>
2020-11-05 14:22:05 -0500

    reformatted viz

--- brainlit/viz/visualize.py
+++ brainlit/viz/visualize.py
@@ -254,29 +254,27 @@ def plot_image_pts(img, voxels, colorbar=False):
     return fig, axes
 
 
-def napari_viewer(img, labels=None, shapes=None, points=None, label_name="Segmentation"):
+def napari_viewer(
+    img, labels=None, shapes=None, points=None, label_name="Segmentation"
+):
     with napari.gui_qt():
         viewer = napari.view_image(np.squeeze(np.array(img)))
         if labels is not None:
             if isinstance(labels, list):
-                for l,label in enumerate(labels):
-                    name = label_name + "_" + str(l) 
+                for l, label in enumerate(labels):
+                    name = label_name + "_" + str(l)
                     viewer.add_labels(label, name=name)
             else:
                 viewer.add_labels(labels, name=label_name)
         if shapes is not None:
             if isinstance(labels, list):
-                for l,shape in enumerate(shapes):
-                    name = label_name + "_" + str(l) 
-                    viewer.add_shapes(
-                        data=shape, shape_type="path", edge_color="blue"
-                    )
+                for l, shape in enumerate(shapes):
+                    name = label_name + "_" + str(l)
+                    viewer.add_shapes(data=shape, shape_type="path", edge_color="blue")
             else:
                 viewer.add_shapes(
                     data=shapes, shape_type="path", edge_color="blue", name="Skeleton"
                 )
         if points is not None:
-            viewer.add_points(
-                points, size=2, face_color='red'
-            )
+            viewer.add_points(points, size=2, face_color="red")
         return viewer
